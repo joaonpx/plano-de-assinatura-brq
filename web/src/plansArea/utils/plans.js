69f0217bf826ed345db13b39a741c832ff6d45b0
Npx <cheyprivado@gmail.com>
2023-09-24 12:15:01 -0300

    :hammer: Atualizando script plans

--- web/src/plansArea/utils/plans.js
+++ web/src/plansArea/utils/plans.js
@@ -1,69 +1,153 @@
-import { createPlanElement } from "./createElement.js"
+import { createHoursPlanElement } from "./createElement.js"
 
-const plans = [
-  {
-    type: "Bronze",
-    date: {
-      days: 1,
+const plans = {
+  hours: [
+    {
+      type: "Bronze",
+      date: {
+        days: 1,
+      },
+      description: "Perfeito para ocasiões especiais",
+      benefits: ["Delivery", "Condutor adicional"],
     },
-    description: "Perfeito para ocasiões especiais",
-    benefits: ["Delivery", "Condutor adicional"],
-  },
-  {
-    type: "Prata",
-    date: {
-      days: 3,
+    {
+      type: "Prata",
+      date: {
+        days: 3,
+      },
+      description: "Ideal para curtas viagens",
+      benefits: [
+        "Delivery",
+        "Condutor adicional",
+        "Programa de pontuação",
+        "Notificações personalizadas",
+      ],
     },
-    description: "Ideal para curtas viagens",
-    benefits: [
-      "Delivery",
-      "Condutor adicional",
-      "Programa de pontuação",
-      "Notificações personalizadas",
-    ],
-  },
-  {
-    type: "Ouro",
-    date: {
-      days: 5,
+    {
+      type: "Ouro",
+      date: {
+        days: 5,
+      },
+      description: "Ideal para curtas viagens",
+      benefits: [
+        "Delivery",
+        "Condutor adicional",
+        "Programa de pontuação",
+        "Notificações personalizadas",
+        "Troca de veículo flexivel",
+        "Motorista particular",
+      ],
     },
-    description: "Ideal para curtas viagens",
-    benefits: [
-      "Delivery",
-      "Condutor adicional",
-      "Programa de pontuação",
-      "Notificações personalizadas",
-      "Troca de veículo flexivel",
-      "Motorista particular",
-    ],
-  },
-  // {
-  //   type: "Personalizado",
-  //   date: {
-  //     days: 0,
-  //   },
-  //   description: "Personalize sua assinatura conforme sua necessidade:",
-  //   benefits: [
-  //     "Delivery",
-  //     "Condutor adicional",
-  //     "Programa de pontuação",
-  //     "Notificações personalizadas",
-  //     "Troca de veículo flexivel",
-  //     "Motorista particular",
-  //   ],
-  // },
-]
+    {
+      type: "Personalizado",
+      date: {
+        days: 0,
+      },
+      description: "Personalize sua assinatura conforme sua necessidade:",
+      benefits: [
+        "Delivery",
+        "Condutor adicional",
+        "Programa de pontuação",
+        "Notificações personalizadas",
+        "Troca de veículo flexivel",
+        "Motorista particular",
+      ],
+    },
+  ],
+
+  monthly: [
+    {
+      type: "Bronze",
+      date: {
+        time: "Mensal",
+      },
+      description: "Perfeito para ocasiões especiais",
+      benefits: ["Delivery", "Condutor adicional"],
+    },
+    {
+      type: "Prata",
+      date: {
+        time: "Trimestral",
+      },
+      description: "Ideal para curtas viagens",
+      benefits: [
+        "Delivery",
+        "Condutor adicional",
+        "Programa de pontuação",
+        "Notificações personalizadas",
+      ],
+    },
+    {
+      type: "Ouro",
+      date: {
+        time: "Semestral",
+      },
+      description: "Ideal para curtas viagens",
+      benefits: [
+        "Delivery",
+        "Condutor adicional",
+        "Programa de pontuação",
+        "Notificações personalizadas",
+        "Troca de veículo flexivel",
+        "Motorista particular",
+      ],
+    },
+    {
+      type: "Personalizado",
+      date: {
+        time: 0,
+      },
+      description: "Personalize sua assinatura conforme sua necessidade:",
+      benefits: [
+        "Delivery",
+        "Condutor adicional",
+        "Programa de pontuação",
+        "Notificações personalizadas",
+        "Troca de veículo flexivel",
+        "Motorista particular",
+      ],
+    },
+  ],
+}
 
 function updatePlans() {
-  plans.forEach((plan) => {
-    const planElement = createPlanElement(
-      plan.type,
-      plan.date.days,
-      plan.description,
-      plan.benefits
-    )
+  const hoursCheckbox = document.querySelector(".hours")
+  const monthlyCheckbox = document.querySelector(".monthly")
 
-    document.querySelector(".plans").appendChild(planElement)
+  const checkInputs = document.querySelectorAll(".form-check .form-check-input")
+
+  if (hoursCheckbox.checked) {
+    plans.hours.forEach((plan) => {
+      const planElement = createHoursPlanElement(
+        plan.type,
+        plan.date.days,
+        plan.description,
+        plan.benefits
+      )
+
+      document.querySelector(".plans").appendChild(planElement)
+    })
+  }
+
+  checkInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+      if (input == hoursCheckbox) {
+        plans.hours.forEach((plan) => {
+          const planElement = createHoursPlanElement(
+            plan.type,
+            plan.date.days,
+            plan.description,
+            plan.benefits
+          )
+
+          document.querySelector(".plans").appendChild(planElement)
+        })
+      } else if (input == monthlyCheckbox) {
+        plans.monthly.forEach((plan) => {
+          console.log("change to monthly")
+        })
+      }
+    })
   })
 }
 
