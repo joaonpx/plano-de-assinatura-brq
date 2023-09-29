21dbdb66469aeb9e2185e773a3075a5412368d39
Davi Santana <davi200123@gmail.com>
2023-09-29 19:48:59 -0300

    filters creation and cars card area att

--- web/src/carsArea/index.js
+++ web/src/carsArea/index.js
@@ -1,67 +1,76 @@
 const items = [
-  {
-    img: "../../public/carFilters/todos.svg",
-    label: "Todos",
-  },
-  {
-    img: "../../public/carFilters/economico.svg",
-    label: "Economico",
-  },
-  {
-    img: "../../public/carFilters/intermediario.svg",
-    label: "Intermediário",
-  },
-  {
-    img: "../../public/carFilters/familia.svg",
-    label: "Familia",
-  },
-  {
-    img: "../../public/carFilters/suv.svg",
-    label: "Suv",
-  },
-  {
-    img: "../../public/carFilters/executivo.svg",
-    label: "Executivo",
-  },
-  {
-    img: "../../public/carFilters/utilitario.svg",
-    label: "Utilitarios",
-  },
+  { img: "todos.svg", label: "Todos" },
+  { img: "economico.svg", label: "Economico" },
+  { img: "inter.svg", label: "Intermediário" },
+  { img: "familia.svg", label: "Familia" },
+  { img: "suv.svg", label: "Suv" },
+  { img: "executivo.svg", label: "Executivo" },
+  { img: "utilitario.svg", label: "Utilitarios" },
 ];
 
-function createImageLabelComponent(item) {
-  // Cria elementos HTML para a imagem e a label
-  const componentContainer = document.createElement("div");
+function CreateImageLabelComponent(item) {
+  const componentContainer = document.createElement("li");
   const imageElement = document.createElement("img");
   const labelElement = document.createElement("label");
 
-  // Define o atributo src da imagem com a URL da imagem do objeto item
-  imageElement.src = item.img;
-
-  // Define o texto da label com o texto do objeto item
+  imageElement.src = `../../public/carFilters/${item.img}`;
   labelElement.textContent = item.label;
 
-  // Adiciona a imagem e a label ao container
+  labelElement.className =
+    "content-center flex justify-center bg-white h-auto w-full h-full";
+
+  imageElement.className =
+    "content-center flex justify-center bg-black p-0 h-auto w-full";
+
+  componentContainer.className =
+    "content-center flex flex-col bg-yellow-200 w-full h-full";
+
   componentContainer.appendChild(imageElement);
   componentContainer.appendChild(labelElement);
 
-  // Retorna o container do componente
   return componentContainer;
 }
 
-// Itera sobre o array items e cria componentes para cada item
-items.forEach((item) => {
-  const component = createImageLabelComponent(item);
+const containerDiv = document.getElementById("component-container");
 
-  // Encontre a div específica onde você deseja inserir o componente
-  const containerDiv = document.getElementById("component-container");
-
-  // Adicione o componente à div específica
-  if (containerDiv) {
+if (!containerDiv) {
+  console.error(
+    'A div com id "component-container" não foi encontrada no documento.'
+  );
+} else {
+  items.forEach((item) => {
+    const component = CreateImageLabelComponent(item);
     containerDiv.appendChild(component);
-  } else {
-    console.error(
-      'A div com id "component-container" não foi encontrada no documento.'
-    );
-  }
-});
+  });
+}
+
+const cars = [
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+  { category: "basic", img: "ônix.svg", name: "ônix" },
+];
+
+function CreateCarsCards (cars){
+  const category = "das"
+}
