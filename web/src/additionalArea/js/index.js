0df01a561e9a5b7e228263d792451957e7223109
Npx <cheyprivado@gmail.com>
2023-09-30 16:13:43 -0300

    :bug: Solucionando bug do checkbox

--- web/src/additionalArea/js/index.js
+++ web/src/additionalArea/js/index.js
@@ -1,40 +1,31 @@
-const menuItems = document.querySelectorAll('.menu-item');
-const produtos = document.querySelectorAll('.produto');
+const menuItems = document.querySelectorAll(".menu-item")
+const produtos = document.querySelectorAll(".produto")
 
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        const category = item.getAttribute('data-category');
-        const isActive = item.classList.contains('active'); 
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const category = item.getAttribute("data-category")
+    const isActive = item.classList.contains("active")
 
-        produtos.forEach(produto => {
-            const produtoCategory = produto.getAttribute('data-category');
-            
-            if (isActive || category === 'todos') {
-                
-                produto.style.display = 'block';
-            } else {
-                
-                if (category === produtoCategory) {
-                    produto.style.display = 'block';
-                } else {
-                    produto.style.display = 'none';
-                }
-            }
-        });
+    produtos.forEach((produto) => {
+      const produtoCategory = produto.getAttribute("data-category")
 
-        menuItems.forEach(menuItem => {
-            menuItem.classList.remove('active');
-        });
-
-        if (!isActive) {
-            item.classList.add('active');
+      if (isActive || category === "todos") {
+        produto.style.display = "block"
+      } else {
+        if (category === produtoCategory) {
+          produto.style.display = "block"
+        } else {
+          produto.style.display = "none"
         }
-    });
-});
+      }
+    })
 
-const checkbox = document.querySelector('.custom-checkbox input');
-const checkboxIcon = document.querySelector('.custom-checkbox .bi-check');
+    menuItems.forEach((menuItem) => {
+      menuItem.classList.remove("active")
+    })
 
-        checkbox.addEventListener('change', () => {
-            checkboxIcon.style.display = checkbox.checked ? 'inline-block' : 'none';
-        });
+    if (!isActive) {
+      item.classList.add("active")
+    }
+  })
+})
