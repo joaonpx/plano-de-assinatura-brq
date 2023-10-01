cd642f3004f4225749684f05428d98b42a56556a
Gustavo Dourado <rm551921@fiap.com.br>
2023-10-01 11:43:20 -0300

    atualizando script js

--- web/src/registerArea/index.js
+++ web/src/registerArea/index.js
@@ -1,37 +1,41 @@
-// (function () {
-//   'use strict'
+(function () {
+  'use strict'
 
-//   var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('.needs-validation')
 
-//   Array.prototype.slice.call(forms)
-//     .forEach(function (form) {
-//       form.addEventListener('submit', function (event) {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-// })()
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
-// function showHideDiv()
-// {
-//   const fisicInput = document.getElementById('fisicPerson')
-//   const juridicInput = document.getElementById('juridicPerson')
-//   const selectInputs = document.querySelectorAll('.form-check .form-check-input')
-//   if (fisicInput.checked){
-//     document.getElementById('formPhysical').classList.remove('disabled')
-//   }
-//   selectInputs.forEach((input) => {
-//     input.addEventListener('change', () => {
-//       if (input != fisicInput){
-//         document.getElementById('formPhysical').classList.add('disabled')
-//         document.getElementById('formJuridic').classList.remove('disabled')
-//       }
-//     })
-//   })
-// }
+function updateForms()
+{
+  const fisicInput = document.getElementById('fisicPerson')
+  const juridicInput = document.getElementById('juridicPerson')
+  const selectInputs = document.querySelectorAll('.form-check .form-check-input')
+  if (fisicInput.checked){
+    document.getElementById('formPhysical').classList.remove('disabled')
+  }
+  selectInputs.forEach((input) => {
+    input.addEventListener('change', () => {
+      if (input != fisicInput){
+        document.getElementById('formPhysical').classList.add('disabled')
+        document.getElementById('formJuridic').classList.remove('disabled')
+      }
+      else if(input == fisicInput){
+        document.getElementById('formJuridic').classList.add('disabled')
+        document.getElementById('formPhysical').classList.remove('disabled')
+      }
+    })
+  })
+}
 
-// showHideDiv()
+updateForms()
