53a31eca40eaf64ae90f803204875fa2cc9bb6a7
Npx <cheyprivado@gmail.com>
2023-10-01 12:53:39 -0300

    :recycle: Refatorando

--- web/src/registerArea/index.js
+++ web/src/registerArea/index.js
@@ -1,38 +1,42 @@
-(function () {
-  'use strict'
+;(function () {
+  "use strict"
 
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll(".needs-validation")
 
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
         }
 
-        form.classList.add('was-validated')
-      }, false)
-    })
+        form.classList.add("was-validated")
+      },
+      false
+    )
+  })
 })()
 
-function updateForms()
-{
-  const fisicInput = document.getElementById('fisicPerson')
-  const juridicInput = document.getElementById('juridicPerson')
-  const selectInputs = document.querySelectorAll('.form-check .form-check-input')
-  if (fisicInput.checked){
-    document.getElementById('formPhysical').classList.remove('disabled')
+function updateForms() {
+  const fisicInput = document.getElementById("fisicPerson")
+  const selectInputs = document.querySelectorAll(
+    ".form-check .form-check-input"
+  )
+
+  if (fisicInput.checked) {
+    document.getElementById("formPhysical").classList.remove("disabled")
   }
+
   selectInputs.forEach((input) => {
-    input.addEventListener('change', () => {
-      if (input != fisicInput){
-        document.getElementById('formPhysical').classList.add('disabled')
-        document.getElementById('formJuridic').classList.remove('disabled')
-      }
-      else if(input == fisicInput){
-        document.getElementById('formJuridic').classList.add('disabled')
-        document.getElementById('formPhysical').classList.remove('disabled')
+    input.addEventListener("change", () => {
+      if (input != fisicInput) {
+        document.getElementById("formPhysical").classList.add("disabled")
+        document.getElementById("formJuridic").classList.remove("disabled")
+      } else if (input == fisicInput) {
+        document.getElementById("formJuridic").classList.add("disabled")
+        document.getElementById("formPhysical").classList.remove("disabled")
       }
     })
   })
