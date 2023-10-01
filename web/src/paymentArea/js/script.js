0ac6b601fa37306bb8834014ba5c7dade597fef7
Fernanda Nalon <fernanda.nalon15@gmail.com>
2023-10-01 12:54:36 -0300

    Padronizando layout payment-area

--- web/src/paymentArea/js/script.js
+++ web/src/paymentArea/js/script.js
@@ -12,6 +12,7 @@ function toggleDiv(divId) {
 
 document.getElementById('button1').addEventListener('click', function() {
     toggleDiv('div1');
+    
 });
 
 document.getElementById('button2').addEventListener('click', function() {
@@ -21,3 +22,20 @@ document.getElementById('button2').addEventListener('click', function() {
 document.getElementById('button3').addEventListener('click', function() {
     toggleDiv('div3');
 });
+
+
+let ultimoInput = null;
+
+function mudarBackground(input) {
+    if (ultimoInput !== null) {
+        ultimoInput.style.backgroundColor = "#1c1c1e";
+        ultimoInput.style.color = "white";
+    }
+
+    input.style.backgroundColor = "white";
+    input.style.color = "black";
+    ultimoInput = input;
+}
+
+document.getElementById("button1").click();
+document.getElementById("button1").mudarBackground();
